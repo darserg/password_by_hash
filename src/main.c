12b728ead84d1828553085937c26ab2ca4ed6184
darserg <darserg1212@mail.ru>
2026-09-25 12:53:12 +0400

    feat: string checking

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
 
 // hash
 #include "hash/md5-c.h"
@@ -7,13 +10,36 @@
 // password variants
 
 // compare
+int len(char *S) {
+	uint16_t i = 0;
+
+	while (S[i])
+		i++;
+
+	return i;
+}
+
+bool is_equal(char *a, char *b) {
+	if (len(a) != len(b))
+		return false;
+
+	int i = 0;
+
+	while (a[i]) {
+		if (a[i] != b[i])
+			return false;
+
+		i++;
+	}
+
+	return true;
+}
 
 int main(int argc, char *argv[])
 {
     char *hash = argv[1];
     char *alph = argv[2];
-    int n = atoi(argv[3]);
+    uint8_t n = atoi(argv[3]);
 
-    printf("%d", n + 1);
     return 0;
 }
